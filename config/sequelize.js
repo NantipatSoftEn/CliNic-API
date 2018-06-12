6da7efcef965f6e66da05aab34c6a328d5bb5c87
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-06-12 13:31:11 +0700

    fix  relationship :egg: :egg:

--- config/sequelize.js
+++ config/sequelize.js
@@ -20,34 +20,69 @@ sequelize.authenticate()
 
 // create table
 // sequelize.sync();
-// sequelize.sync()
-//     .then(() => model.users.create({
-//         id: '',
-//         name: 'arm',
-//         lastname: 'horyshit',
-//         telephone: '08878',
-//         gender: 'male',
-//         symptomId: 1,
-//         typeUser: 1,
-//         status: 1,
-//         createdAt: '',
-//         updatedAt: ''
-//     }));
+sequelize.sync()
+    .then(() => model.users.create({
+        id: '',
+        name: 'arm',
+        lastname: 'horyshit',
+        telephone: '08878',
+        gender: 'male',
+        symptomId: 1,
+        typeUser: 1,
+        status: 1,
+        createdAt: '',
+        updatedAt: ''
+    }));
 
-// sequelize.sync()
-//     .then(() => model.symptom.create({
-//         id: '',
-//         symptomname: 'headage',
-//         other: 'sdfdsfs'
-//     }));
+sequelize.sync()
+    .then(() => model.symptom.create({
+        id: '',
+        symptomname: 'headage',
+        other: 'sdfdsfs'
+    }));
+
+    sequelize.sync()
+    .then(() => model.period.create({
+        id: '',
+        periodTime: '2012-06-18 16:26:21',
+        maxUser: 10,
+        countUser:2, 
+        createdAt: '',
+        updatedAt: ''
+    }));
+
+sequelize.sync()
+    .then(() => model.reseration.create({
+        id: '',
+        queue: 1,
+        periodId: 1,
+        userId:1, 
+        createdAt: '',
+        updatedAt: ''
+    }));
+
 const op = Sequelize.Op;
 
 const model = {};
 model.Sequelize = Sequelize;
 model.sequelize = sequelize;
 model.op = op;
+
 model.users = require('../model/User')(sequelize, Sequelize);
 model.symptom = require('../model/Symptom')(sequelize, Sequelize);
-model.users.belongsTo(model.symptom);
+model.reseration = require('../model/Reseration')(sequelize, Sequelize);
+model.period = require('../model/Period')(sequelize, Sequelize);
+
+
 model.symptom.hasOne(model.users);
+model.users.belongsTo(model.symptom); // take   symptom_id put user model
+
+
+model.users.hasOne(model.reseration);
+model.reseration.belongsTo(model.users) // take  user_id  put  reseration
+
+
+model.period.hasMany(model.reseration); // take  period_id put reseration
+model.reseration.belongsTo(model.period);
+
 module.exports = model;
