11f2ac294d3748609c88220e033fae415ffd852d
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-06-12 10:58:51 +0700

    config bodyparser

--- app.js
+++ app.js
@@ -14,13 +14,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use('/', indexRouter);
 
 
