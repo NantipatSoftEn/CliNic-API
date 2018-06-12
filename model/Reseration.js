819afc8d5517dcd1893cf95c829d76156e9690e9
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-06-12 13:30:49 +0700

    new model :beers: :beers:

---
+++ model/Reseration.js
@@ -0,0 +1,44 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('reseration', {
+        id: {
+            field: 'id',
+            type: DataTypes.INTEGER(10).UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        queue: {
+            field: 'queue',
+            type: DataTypes.STRING(191),
+            primaryKey: false,
+            allowNull: false,
+            autoIncrement: false,
+        },
+        periodId:{
+            field: 'period_id',
+            type: DataTypes.INTEGER(10).UNSIGNED,
+            primaryKey: true,
+            allowNull: false 
+        },
+        userId:{
+            field: 'user_id',
+            type: DataTypes.INTEGER(10).UNSIGNED,
+            primaryKey: true,
+            allowNull: false 
+        },
+        createdAt: {
+            field: 'created_at',
+            type: DataTypes.DATE,
+            primaryKey: false,
+            autoIncrement: false,
+        },
+        updatedAt: {
+            field: 'updated_at',
+            type: DataTypes.DATE,
+            primaryKey: false,
+            autoIncrement: false,
+        },
+    });
+}
