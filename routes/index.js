f2fceaeed168f5598c5c12f2e93d28ad0000b6b0
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-06-13 18:54:36 +0700

    add route

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
-var express  = require('express')
+var express = require('express')
 var Resource = require('express-resource')
 var app = express();
 
- //var router = express.Router();
+//var router = express.Router();
 
-var userServices          = require('../service/UserServices');
+var userServices = require('../service/UserServices');
 var reserationServices = require('../service/ReserationServices');
+var symptomServices = require('../service/SymptomServices');
+var profilepatentServices = require('../service/ProfilePatentServices');
+var medicineServices = require('../service/MedicineServices');
+var periodServices = require('../service/PeriodServices');
+var datecloseServices = require('../service/DateCloseServices');
 
-app.resource('user', userServices );
-app.resource('reseration',  reserationServices );
-
+app.resource('user', userServices);
+app.resource('reseration', reserationServices);
+app.resource('symptom', symptomServices);
+app.resource('profilepatent', profilepatentServices);
+app.resource('medicine', medicineServices);
+app.resource('period',  periodServices);
+app.resource('dateclose', medicineServices);
 //app.resource('', );
 
 
