b1df1905bdd0b266811a73089319e853294a28de
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-06-13 19:10:37 +0700

    fix relationship  :penguin: :penguin:

--- config/sequelize.js
+++ config/sequelize.js
@@ -16,6 +16,62 @@ sequelize.authenticate()
     .catch(err => {
         console.error('Unable to connect to the database:', err);
     });
+    sequelize.sync()
+    .then(() => model.symptom.create({
+        id: '',
+        symptomname: 'headage',
+        other: 'sdfdsfs'
+    }))
+    .then(() => model.users.create({
+        id: '',
+        name: 'arm',
+        lastname: 'horyshit',
+        telephone: '08878',
+        gender: 'male',
+        symptomId: 1,
+        typeUser: 1,
+        status: 1,
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.period.create({
+        id: '',
+        periodTime: '2012-06-18 16:26:21',
+        maxUser: 10,
+        countUser: 2,
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.reseration.create({
+        id: '',
+        queue: 1,
+        userId: 1,
+        periodId: 1,
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.dateclose.create({
+        id: '',
+        date: '2012-06-18 16:26:21',
+        house: '16:26:21',
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.medicine.create({
+        id: '',
+        medicineName: 'parasettamal',
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.profilepatent.create({
+        id: '',
+        userId: 1,
+        medicineId: 1,
+        diagnose: 'Not ok',
+        createdAt: '',
+        updatedAt: ''
+    }))
+
 
 const op = Sequelize.Op;
 
@@ -43,9 +99,9 @@ model.period.hasMany(model.reseration); //  take periodId to reseration
 model.reseration.belongsTo(model.users);
 model.users.hasMany(model.reseration); // take userId to reseration
 
-model.users.belongsTo(model.profilepatent);
-model.profilepatent.hasMany(model.users);
-
+model.profilepatent.belongsTo(model.users);
+model.users.hasMany(model.profilepatent); // take profilepatent to user
+ 
 model.profilepatent.belongsTo(model.medicine);
 model.medicine.hasMany(model.profilepatent); //  take medicineId to profilepatent
 module.exports = model;
