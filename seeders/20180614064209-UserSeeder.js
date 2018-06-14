3c6f19b0010c8be634253476631837c091519c1a
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-06-14 20:09:21 +0700

    npm install faker

--- seeders/20180614064209-UserSeeder.js
+++ seeders/20180614064209-UserSeeder.js
@@ -1,12 +1,12 @@
 'use strict';
-
+var faker = require('faker');
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
     */
-    var data = {
+    var data = [{
       id: '',
       name: 'seeder',
       lastname: 'seeder',
@@ -17,8 +17,19 @@ module.exports = {
       status: 1,
       created_at: new Date(),
       updated_at: new Date()
-    };
-    return queryInterface.bulkInsert('users', [data], {});
+    }, {
+      id: '',
+      name: 'seeder2',
+      lastname: 'seeder2',
+      telephone: 'seeder2',
+      gender: 'male',
+      symptom_id: 1,
+      typeUser: 2,
+      status: 2,
+      created_at: new Date(),
+      updated_at: new Date()
+    }];
+    return queryInterface.bulkInsert('users', data, {});
   },
 
   down: (queryInterface, Sequelize) => {
