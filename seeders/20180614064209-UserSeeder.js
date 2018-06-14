9badf10d755a89d0aa7bf94d674d1f741411568e
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-06-15 03:52:39 +0700

    :robot:  :bookmark:  :pencil2:  fakerdata

--- seeders/20180614064209-UserSeeder.js
+++ seeders/20180614064209-UserSeeder.js
@@ -1,34 +1,40 @@
 'use strict';
 var faker = require('faker');
+
+var data = [];
+
+var gender = ['male','female'];
+var symptom_id = [1]
+var typeUser = [1,2,3];
+var status =[1,2,3];
+
+for(var i=0;i<5;i++){
+
+  var genderRamdom = gender[Math.floor(Math.random()*gender.length)];
+  var symptom_idRamdom = symptom_id[Math.floor(Math.random()*symptom_id.length)];
+  var typeUserRamdom = typeUser[Math.floor(Math.random()*typeUser.length)];
+  var statusRamdom = status[Math.floor(Math.random()*status.length)];
+   data[i] = {
+    id: '',
+    name: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    telephone: faker.phone.phoneNumber(),
+    gender: genderRamdom,
+    symptom_id:symptom_idRamdom,
+    typeUser: typeUserRamdom,
+    status: statusRamdom,
+    created_at: new Date(),
+    updated_at: new Date()
+  };
+}
+
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
     */
-    var data = [{
-      id: '',
-      name: 'seeder',
-      lastname: 'seeder',
-      telephone: 'seeder',
-      gender: 'male',
-      symptom_id: 1,
-      typeUser: 1,
-      status: 1,
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      id: '',
-      name: 'seeder2',
-      lastname: 'seeder2',
-      telephone: 'seeder2',
-      gender: 'male',
-      symptom_id: 1,
-      typeUser: 2,
-      status: 2,
-      created_at: new Date(),
-      updated_at: new Date()
-    }];
     return queryInterface.bulkInsert('users', data, {});
   },
 
