4847f4120175b2b6c6c79a7a1a4a274b4418d821
PanupongDeve <5635512110@psu.ac.th>
2018-06-16 18:54:39 +0700

    updated

--- configdb/sequelize.js
+++ configdb/sequelize.js
@@ -46,6 +46,62 @@ model.medicine.hasMany(model.profilepatent); //  take medicineId to profilepaten
 
 
 sequelize.sync()
+    .then(() => model.symptom.create({
+        id: '',
+        symptomname: 'headage',
+        other: 'sdfdsfs'
+    }))
+    .then(() => model.users.create({
+        id: '',
+        name: 'arm',
+        lastname: 'horyshit',
+        telephone: '08878',
+        gender: 'male',
+        symptomId: 1,
+        typeUser: 1,
+        status: 1,
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.period.create({
+        id: '',
+        periodTime: '2012-06-18 16:26:21',
+        maxUser: 10,
+        countUser: 2,
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.reseration.create({
+        id: '',
+        queue: 1,
+        userId: 1,
+        periodId: 1,
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.dateclose.create({
+        id: '',
+        date: '2012-06-18 16:26:21',
+        house: '16:26:21',
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.medicine.create({
+        id: '',
+        medicineName: 'parasettamal',
+        createdAt: '',
+        updatedAt: ''
+    }))
+    .then(() => model.profilepatent.create({
+        id: '',
+        userId: 1,
+        medicineId: 1,
+        diagnose: 'Not ok',
+        createdAt: '',
+        updatedAt: ''
+    }))
+
+
 
 
 module.exports = model;
