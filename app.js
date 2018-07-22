de1155cf1ec12b191eca8a096136787f967812c2
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-07-22 11:28:56 +0700

    config

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ var app = express();
 
 // view engine setup
 app.use(cors())
+app.engine('ejs', require('express-ejs-extend'));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');  
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
