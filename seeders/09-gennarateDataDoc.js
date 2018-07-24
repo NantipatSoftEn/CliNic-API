a5861cfe8f0b5ee85dd65ee1e3bbc542ced39307
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-07-24 08:50:03 +0700

    make data seeder

--- seeders/09-gennarateDataDoc.js
+++ seeders/09-gennarateDataDoc.js
@@ -1,31 +1,28 @@
 'use strict';
 var data = [];
-for (var i = 0; i < 5; i++) {
+
+
+var  url     = ['/user' ,'/user'  ,'/user/:id'  ,'/user/:id'  ,'/user/:id',
+                    '/symptom' ,'/symptom'  ,'/symptom/:id'  ,'/symptom/:id'  ,'/symptom/:id',
+                    '/reseration' ,'/reseration'  ,'/reseration/:id'  ,'/reseration/:id'  ,'/reseration/:id',
+                    '/profilepatent' ,'/profilepatent'  ,'/profilepatent/:id'  ,'/profilepatent/:id'  ,'/profilepatent/:id',
+                    '/medicine' ,'/medicine'  ,'/medicine/:id'  ,'/medicine/:id'  ,'/medicine/:id',
+                    '/period' ,'/period'  ,'/period/:id'  ,'/period/:id'  ,'/period/:id',
+                    '/dateclose' ,'/dateclose'  ,'/dateclose/:id'  ,'/dateclose/:id'  ,'/dateclose/:id',]
+
+
+
+var action = ['GET' ,'POST','GET' ,'PUT '  ,'DELETE ']
+
+var param = [ '']
+for (var i = 0; i < ; i++) {
   data[i] = {
     id: '',
+    url: url[i],
     action: 'GET',
     pathparameters: 'non-required',
-    respone:`[
-        {
-            "id": 1,
-            "name": "testasdsadadsd",
-            "lastname": "sdasd",
-            "telephone": "848484",
-            "gender": "female",
-            "symptomId": 1,
-            "typeUser": 1,
-            "status": "1",
-            "createdAt": "2018-06-20T11:11:13.000Z",
-            "updatedAt": "2018-06-22T03:34:27.000Z",
-            "symptom": {
-                "id": 1,
-                "symptomname": "allergy",
-                "other": "Eveniet et voluptatem nobis illum est consequuntur fugiat mollitia.",
-                "createdAt": "2018-06-20T11:11:13.000Z",
-                "updatedAt": "2018-06-20T11:11:13.000Z"
-            }
-        }
-    ]`,
+    respone:'200 Success',
+    data: null,
     created_at: new Date(),
     updated_at: new Date()
   };
