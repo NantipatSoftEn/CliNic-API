f8da5a5cdecb416fdbc873ef5c6fd5a2d61f48ae
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-07-24 10:25:04 +0700

    sucess

--- seeders/09-gennarateDataDoc.js
+++ seeders/09-gennarateDataDoc.js
@@ -8,7 +8,7 @@ var  url     = ['/user' ,'/user'  ,'/user/:id'  ,'/user/:id'  ,'/user/:id',
                     '/profilepatent' ,'/profilepatent'  ,'/profilepatent/:id'  ,'/profilepatent/:id'  ,'/profilepatent/:id',
                     '/medicine' ,'/medicine'  ,'/medicine/:id'  ,'/medicine/:id'  ,'/medicine/:id',
                     '/period' ,'/period'  ,'/period/:id'  ,'/period/:id'  ,'/period/:id',
-                    '/dateclose' ,'/dateclose'  ,'/dateclose/:id'  ,'/dateclose/:id'  ,'/dateclose/:id']
+                    '/dateclose' ,'/dateclose'  ,'/dateclose/:id'  ,'/dateclose/:id'  ,'/dateclose/:id'];
 
 var action = [
   'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
@@ -18,7 +18,7 @@ var action = [
   'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
   'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
   'GET' ,'POST','GET' ,'PUT '  ,'DELETE '
-]
+];
 var pathparameters = [
                                     'non-required','non-required','userId','userId','userId',
                                     'non-required','non-required','symptomId','symptomId','symptomId',
@@ -27,29 +27,259 @@ var pathparameters = [
                                     'non-required','non-required','medicineId','medicineId','medicineId',
                                     'non-required','non-required','periodId','periodId','periodId',
                                     'non-required','non-required','datecloseId','datecloseId','datecloseId'
-                                  ]
+                                  ];
 
-
-var dataReturn =[  `{
-      "id": 1,
-      "name": "arm",
-      "lastname": "horyshit",
-      "telephone": "08878",
-      "gender": "male",
-      "symptomId": 1,
-      "typeUser": 1,
-      "status": "1",
-      "createdAt": "2018-07-23T23:59:58.000Z",
-      "updatedAt": "2018-07-23T23:59:58.000Z",
-      "symptom": {
-          "id": 1,
-          "symptomname": "headage",
-          "other": "sdfdsfs",
-          "createdAt": "2018-07-23T23:58:09.000Z",
-          "updatedAt": "2018-07-23T23:58:09.000Z"
-      }'
-  }`, ]
-
+                                  var dataReturn = [
+                                    `{
+                                  "id": 1,
+                                  "name": "arm",
+                                  "lastname": "horyshit",
+                                  "telephone": "08878",
+                                  "gender": "male",
+                                  "symptomId": 1,
+                                  "typeUser": 1,
+                                  "status": "1",
+                                  "createdAt": "2018-07-23T23:59:58.000Z",
+                                  "updatedAt": "2018-07-23T23:59:58.000Z",
+                                  "symptom": {
+                                      "id": 1,
+                                      "symptomname": "headage",
+                                      "other": "sdfdsfs",
+                                      "createdAt": "2018-07-23T23:58:09.000Z",
+                                      "updatedAt": "2018-07-23T23:58:09.000Z"
+                                  }'
+                                }`,
+                                    `{
+                                  "id": 1,
+                                  "name": "arm",
+                                  "lastname": "horyshit",
+                                  "telephone": "08878",
+                                  "gender": "male",
+                                  "symptomId": 1,
+                                  "typeUser": 1,
+                                  "status": "1",
+                                  "createdAt": "2018-07-23T23:59:58.000Z",
+                                  "updatedAt": "2018-07-23T23:59:58.000Z",
+                                  "symptom": {
+                                      "id": 1,
+                                      "symptomname": "headage",
+                                      "other": "sdfdsfs",
+                                      "createdAt": "2018-07-23T23:58:09.000Z",
+                                      "updatedAt": "2018-07-23T23:58:09.000Z"
+                                  }'
+                                }`,
+                                    `{
+                                  "id": 1,
+                                  "name": "arm",
+                                  "lastname": "horyshit",
+                                  "telephone": "08878",
+                                  "gender": "male",
+                                  "symptomId": 1,
+                                  "typeUser": 1,
+                                  "status": "1",
+                                  "createdAt": "2018-07-23T23:59:58.000Z",
+                                  "updatedAt": "2018-07-23T23:59:58.000Z",
+                                  "symptom": {
+                                      "id": 1,
+                                      "symptomname": "headage",
+                                      "other": "sdfdsfs",
+                                      "createdAt": "2018-07-23T23:58:09.000Z",
+                                      "updatedAt": "2018-07-23T23:58:09.000Z"
+                                  }'
+                                }`,
+                                    'sucess',
+                                    'sucess',
+                                    'sucess',
+                                    ` {
+                                    "id": 1,
+                                    "symptomname": "appendicitis",
+                                    "other": "Placeat libero laudantium sit error quos dolorem.",
+                                    "createdAt": "2018-07-24T02:20:54.000Z",
+                                    "updatedAt": "2018-07-24T02:20:54.000Z"
+                                }`,
+                                    'sucess',
+                                    'sucess',
+                                    'sucess',
+                                    ` {
+                                  "id": 1,
+                                  "queue": "0",
+                                  "userId": 2,
+                                  "periodId": 1,
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z",
+                                  "period": {
+                                      "id": 1,
+                                      "periodTime": "2018-07-24T02:20:55.000Z",
+                                      "maxUser": 30,
+                                      "countUser": 8,
+                                      "createdAt": "2018-07-24T02:20:55.000Z",
+                                      "updatedAt": "2018-07-24T02:20:55.000Z"
+                                  }
+                                }`,
+                                    ` {
+                                  "id": 1,
+                                  "queue": "0",
+                                  "userId": 2,
+                                  "periodId": 1,
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z",
+                                  "period": {
+                                      "id": 1,
+                                      "periodTime": "2018-07-24T02:20:55.000Z",
+                                      "maxUser": 30,
+                                      "countUser": 8,
+                                      "createdAt": "2018-07-24T02:20:55.000Z",
+                                      "updatedAt": "2018-07-24T02:20:55.000Z"
+                                  }
+                                }`,
+                                    ` {
+                                  "id": 1,
+                                  "queue": "0",
+                                  "userId": 2,
+                                  "periodId": 1,
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z",
+                                  "period": {
+                                      "id": 1,
+                                      "periodTime": "2018-07-24T02:20:55.000Z",
+                                      "maxUser": 30,
+                                      "countUser": 8,
+                                      "createdAt": "2018-07-24T02:20:55.000Z",
+                                      "updatedAt": "2018-07-24T02:20:55.000Z"
+                                  }
+                                }`,
+                                    'sucess',
+                                    'sucess',
+                                    'sucess',
+                                    `"id": 1,
+                                "userId": 4,
+                                "medicineId": 5,
+                                "diagnose": "Saepe qui illum porro commodi.",
+                                "createdAt": "2018-07-24T02:20:55.000Z",
+                                "updatedAt": "2018-07-24T02:20:55.000Z",
+                                "medicine": {
+                                    "id": 5,
+                                    "medicineName": "vitamin",
+                                    "createdAt": "2018-07-24T02:20:55.000Z",
+                                    "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `"id": 1,
+                                "userId": 4,
+                                "medicineId": 5,
+                                "diagnose": "Saepe qui illum porro commodi.",
+                                "createdAt": "2018-07-24T02:20:55.000Z",
+                                "updatedAt": "2018-07-24T02:20:55.000Z",
+                                "medicine": {
+                                    "id": 5,
+                                    "medicineName": "vitamin",
+                                    "createdAt": "2018-07-24T02:20:55.000Z",
+                                    "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `"id": 1,
+                                "userId": 4,
+                                "medicineId": 5,
+                                "diagnose": "Saepe qui illum porro commodi.",
+                                "createdAt": "2018-07-24T02:20:55.000Z",
+                                "updatedAt": "2018-07-24T02:20:55.000Z",
+                                "medicine": {
+                                    "id": 5,
+                                    "medicineName": "vitamin",
+                                    "createdAt": "2018-07-24T02:20:55.000Z",
+                                    "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    'sucess',
+                                    'sucess',
+                                    'sucess',
+                                    `{
+                                  "id": 1,
+                                  "medicineName": "tweezers",
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `{
+                                  "id": 1,
+                                  "medicineName": "tweezers",
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `{
+                                  "id": 1,
+                                  "medicineName": "tweezers",
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    'sucess',
+                                    'sucess',
+                                    'sucess',
+                                    `{
+                                  "id": 1,
+                                  "periodTime": "2018-07-24T02:20:55.000Z",
+                                  "maxUser": 30,
+                                  "countUser": 8,
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `{
+                                    "id": 1,
+                                    "periodTime": "2018-07-24T02:20:55.000Z",
+                                    "maxUser": 30,
+                                    "countUser": 8,
+                                    "createdAt": "2018-07-24T02:20:55.000Z",
+                                    "updatedAt": "2018-07-24T02:20:55.000Z"
+                                  }`,
+                                    `{
+                                    "id": 1,
+                                    "periodTime": "2018-07-24T02:20:55.000Z",
+                                    "maxUser": 30,
+                                    "countUser": 8,
+                                    "createdAt": "2018-07-24T02:20:55.000Z",
+                                    "updatedAt": "2018-07-24T02:20:55.000Z"
+                                  }`,
+                                    'sucess',
+                                    'sucess',
+                                    'sucess',
+                                    `{
+                                  "id": 1,
+                                  "date": "2018-07-24T02:20:55.000Z",
+                                  "house": "16:26:20",
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `{
+                                  "id": 1,
+                                  "periodTime": "2018-07-24T02:20:55.000Z",
+                                  "maxUser": 30,
+                                  "countUser": 8,
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `{
+                                  "id": 1,
+                                  "date": "2018-07-24T02:20:55.000Z",
+                                  "house": "16:26:20",
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `{
+                                  "id": 1,
+                                  "periodTime": "2018-07-24T02:20:55.000Z",
+                                  "maxUser": 30,
+                                  "countUser": 8,
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    `{
+                                  "id": 1,
+                                  "date": "2018-07-24T02:20:55.000Z",
+                                  "house": "16:26:20",
+                                  "createdAt": "2018-07-24T02:20:55.000Z",
+                                  "updatedAt": "2018-07-24T02:20:55.000Z"
+                                }`,
+                                    'sucess',
+                                    'sucess',
+                                    'sucess'
+                                ];
+                                
 for (var i = 0; i < url.length; i++) {
   data[i] = {
     id: '',
@@ -57,7 +287,7 @@ for (var i = 0; i < url.length; i++) {
     action: action[i],
     pathparameters: pathparameters[i],
     respone:'200 Success',
-    data: dataReturn[0],
+    data:    dataReturn[i] ,
     created_at: new Date(),
     updated_at: new Date()
   };
