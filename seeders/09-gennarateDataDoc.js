ae7b96df8fe88def38654e7afbd81bbb398a03f5
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-07-24 10:00:03 +0700

    data

--- seeders/09-gennarateDataDoc.js
+++ seeders/09-gennarateDataDoc.js
@@ -8,21 +8,56 @@ var  url     = ['/user' ,'/user'  ,'/user/:id'  ,'/user/:id'  ,'/user/:id',
                     '/profilepatent' ,'/profilepatent'  ,'/profilepatent/:id'  ,'/profilepatent/:id'  ,'/profilepatent/:id',
                     '/medicine' ,'/medicine'  ,'/medicine/:id'  ,'/medicine/:id'  ,'/medicine/:id',
                     '/period' ,'/period'  ,'/period/:id'  ,'/period/:id'  ,'/period/:id',
-                    '/dateclose' ,'/dateclose'  ,'/dateclose/:id'  ,'/dateclose/:id'  ,'/dateclose/:id',]
+                    '/dateclose' ,'/dateclose'  ,'/dateclose/:id'  ,'/dateclose/:id'  ,'/dateclose/:id']
+
+var action = [
+  'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
+  'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
+  'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
+  'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
+  'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
+  'GET' ,'POST','GET' ,'PUT '  ,'DELETE ',
+  'GET' ,'POST','GET' ,'PUT '  ,'DELETE '
+]
+var pathparameters = [
+                                    'non-required','non-required','userId','userId','userId',
+                                    'non-required','non-required','symptomId','symptomId','symptomId',
+                                    'non-required','non-required','reserationId','reserationId','reserationId',
+                                    'non-required','non-required','profilepatentId','profilepatentId','profilepatentId',
+                                    'non-required','non-required','medicineId','medicineId','medicineId',
+                                    'non-required','non-required','periodId','periodId','periodId',
+                                    'non-required','non-required','datecloseId','datecloseId','datecloseId'
+                                  ]
 
 
+var dataReturn =[  `{
+      "id": 1,
+      "name": "arm",
+      "lastname": "horyshit",
+      "telephone": "08878",
+      "gender": "male",
+      "symptomId": 1,
+      "typeUser": 1,
+      "status": "1",
+      "createdAt": "2018-07-23T23:59:58.000Z",
+      "updatedAt": "2018-07-23T23:59:58.000Z",
+      "symptom": {
+          "id": 1,
+          "symptomname": "headage",
+          "other": "sdfdsfs",
+          "createdAt": "2018-07-23T23:58:09.000Z",
+          "updatedAt": "2018-07-23T23:58:09.000Z"
+      }'
+  }`, ]
 
-var action = ['GET' ,'POST','GET' ,'PUT '  ,'DELETE ']
-
-var param = [ '']
-for (var i = 0; i < ; i++) {
+for (var i = 0; i < url.length; i++) {
   data[i] = {
     id: '',
     url: url[i],
-    action: 'GET',
-    pathparameters: 'non-required',
+    action: action[i],
+    pathparameters: pathparameters[i],
     respone:'200 Success',
-    data: null,
+    data: dataReturn[0],
     created_at: new Date(),
     updated_at: new Date()
   };
